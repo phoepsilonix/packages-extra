cc44aaec947851ce9c4563f3df1c97f4fba2711d
Philip <philm@manjaro.org>
2013-10-08 17:43:24 +0200

    Adjustments - extramodules rebuilds - new packages

--- linux34-extramodules/build-extramodules.sh
+++ linux34-extramodules/build-extramodules.sh
@@ -13,6 +13,7 @@ echo 'building extramodules'
 cd ${pwd}/*bbswitch && makepkg --sign -sf --noconfirm
 cd ${pwd}/*broadcom-wl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*catalyst && makepkg --sign -df --noconfirm
+cd ${pwd}/*catalyst-13.4 && makepkg --sign -df --noconfirm
 cd ${pwd}/*catalyst-legacy && makepkg --sign -df --noconfirm
 cd ${pwd}/*cdfs && makepkg --sign -sf --noconfirm
 cd ${pwd}/*fcpci && makepkg --sign -sf --noconfirm
@@ -23,6 +24,7 @@ cd ${pwd}/*nvidia && makepkg --sign -sf --noconfirm
 cd ${pwd}/*nvidia-304xx && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
+cd ${pwd}/*spl_zfs && makepkg --sign -sf --noconfirm
 cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
 if [ "$(uname -m)" == 'i686' ] ; then
