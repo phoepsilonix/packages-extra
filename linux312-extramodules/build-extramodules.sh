aaedf1ffcd6a2acb7411ec9a3bb58bfa7b7bac3f
Philip <philm@manjaro.org>
2013-12-09 14:06:59 +0100

    Rebuilds - rebuilt against 3.10.23 - rebuilt against 3.1.24 (slmodem and rt3562sta broken)

--- linux312-extramodules/build-extramodules.sh
+++ linux312-extramodules/build-extramodules.sh
@@ -24,6 +24,7 @@ cd ${pwd}/*nvidia && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidia-304xx && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
+cd ${pwd}/*spl_zfs && makepkg --sign -sf --noconfirm
 cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
 if [ "$(uname -m)" == 'i686' ] ; then
