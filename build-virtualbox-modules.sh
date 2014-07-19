39aee19e2a7e013933f39e0c6fc20dde8e1b1ccb
Philip <philm@manjaro.org>
2014-07-19 10:22:01 +0200

    Modifications - extramodules: rebuild against 3.15.6 - extramodules: add acpi_call - extramodules: update vhba-modules - calamares: bump to 0.1.0 - gparted: bump to 0.19.1 - pamac-dev: bump to 0.9.9pre5 - xfce4-settings: update to latest git

--- build-virtualbox-modules.sh
+++ build-virtualbox-modules.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 rm -Rf */*virtualbox-modules/{src,pkg}
-cd linux34*/*virtualbox-modules
-makepkg --sign -df
-cd ../../linux38*/*virtualbox-modules
+cd linux38*/*virtualbox-modules
 makepkg --sign -df
 #cd ../../linux39*/*virtualbox-modules
 #makepkg --sign -df
@@ -13,4 +11,10 @@ makepkg --sign -df
 cd ../../linux312*/*virtualbox-modules
 makepkg --sign -df
 cd ../../linux313*/*virtualbox-modules
+makepkg --sign -df
+cd ../../linux314*/*virtualbox-modules
+makepkg --sign -df
+cd ../../linux315*/*virtualbox-modules
+makepkg --sign -df
+cd ../../linux316*/*virtualbox-modules
 makepkg --sign -df
