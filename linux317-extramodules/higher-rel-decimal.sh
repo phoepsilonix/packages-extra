c7c432af5fd4afa33585205667db0ac2e209a800
Rob McCathie <korrode@gmail.com>
2014-09-10 03:53:07 +1000

    hdecimal pkgrel script update - head command is pointless since adding -m1 to grep - whoops

--- linux317-extramodules/higher-rel-decimal.sh
+++ linux317-extramodules/higher-rel-decimal.sh
@@ -4,7 +4,7 @@ for pkg in $(ls */PKGBUILD | sed s'|/PKGBUILD||g') ; do
    echo "Edit ${pkg}"
    pushd ${pkg} &>/dev/null
    _rel=$(cat PKGBUILD | grep -m1 pkgrel= | cut -d= -f2 | cut -d. -f1)
-   _decrel=$(cat PKGBUILD | grep -m1 pkgrel= | cut -d. -f2 | head -n1)
+   _decrel=$(cat PKGBUILD | grep -m1 pkgrel= | cut -d. -f2)
    sed -i -e "s/pkgrel=${_rel}.${_decrel}/pkgrel=${_rel}.$((${_decrel}+1))/" PKGBUILD
    popd &>/dev/null
 done
