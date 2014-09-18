fd36a509222cf0493940bdfa9d6364e37d81b55a
Philip <philm@manjaro.org>
2014-09-18 14:28:45 +0200

    Updates - bump catalyst to 14.201 - rebuild extramodules as needed - remove extramodules for linux38 & 311

--- build-catalyst.sh
+++ build-catalyst.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 rm -Rf */*catalyst/{src,pkg}
-cd linux34*/*catalyst
-makepkg --sign -df
-cd ../../linux38*/*catalyst
-makepkg --sign -df
-#cd ../../linux39*/*catalyst
-#makepkg --sign -df
-cd ../../linux310*/*catalyst
-makepkg --sign -df
-cd ../../linux311*/*catalyst
+cd linux310*/*catalyst
 makepkg --sign -df
 cd ../../linux312*/*catalyst
 makepkg --sign -df
 cd ../../linux313*/*catalyst
 makepkg --sign -df
 cd ../../linux314*/*catalyst
+makepkg --sign -df
+cd ../../linux315*/*catalyst
+makepkg --sign -df
+cd ../../linux316*/*catalyst
+makepkg --sign -df
+cd ../../linux317*/*catalyst
 makepkg --sign -df
