c1b583fe7f0427da2502972b09705ab6048abe8c
Philip <philm@manjaro.org>
2014-09-23 09:36:13 +0200

    Modifications - update nvidia to 343.22 - xfce4-power-manager: add new needed patch - pamac: update to newer snapshot

--- build-nvidia.sh
+++ build-nvidia.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 rm -Rf */*nvidia/{src,pkg}
-cd linux38*/*nvidia
-makepkg --sign -df
-#cd ../../linux39*/*nvidia
-#makepkg --sign -df
-cd ../../linux310*/*nvidia
-makepkg --sign -df
-cd ../../linux311*/*nvidia
+cd linux310*/*nvidia
 makepkg --sign -df
 cd ../../linux312*/*nvidia
 makepkg --sign -df
@@ -17,4 +11,6 @@ makepkg --sign -df
 cd ../../linux315*/*nvidia
 makepkg --sign -df
 cd ../../linux316*/*nvidia
+makepkg --sign -df
+cd ../../linux317*/*nvidia
 makepkg --sign -df
