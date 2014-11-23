326ad3f82ceb79cf7fd340bab7b7b4b7b400e02c
Philip <philm@manjaro.org>
2014-11-23 09:27:32 +0100

    ManjaroISO: bump to 0.8.11rc2

--- build-virtualbox-modules.sh
+++ build-virtualbox-modules.sh
@@ -8,8 +8,6 @@ cd ../../linux313*/*virtualbox-modules
 makepkg --sign -df
 cd ../../linux314*/*virtualbox-modules
 makepkg --sign -df
-cd ../../linux315*/*virtualbox-modules
-makepkg --sign -df
 cd ../../linux316*/*virtualbox-modules
 makepkg --sign -df
 cd ../../linux317*/*virtualbox-modules
