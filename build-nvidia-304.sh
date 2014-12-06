97fdf5b41bfb2c6b76693681a195d9428982a9dc
Philip <philm@manjaro.org>
2014-12-06 13:44:45 +0100

    Update Nvidia drivers

--- build-nvidia-304.sh
+++ build-nvidia-304.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 rm -Rf */*nvidia*304*/{src,pkg}
-cd linux38*/*nvidia*304*
-makepkg --sign -df
-#cd ../../linux39*/*nvidia*304*
-#makepkg --sign -df
-cd ../../linux310*/*nvidia*304*
-makepkg --sign -df
-cd ../../linux311*/*nvidia*304*
+cd linux310*/*nvidia*304*
 makepkg --sign -df
 cd ../../linux312*/*nvidia*304*
 makepkg --sign -df
@@ -14,7 +8,7 @@ cd ../../linux313*/*nvidia*304*
 makepkg --sign -df
 cd ../../linux314*/*nvidia*304*
 makepkg --sign -df
-cd ../../linux315*/*nvidia*304*
-makepkg --sign -df
 cd ../../linux316*/*nvidia*304*
+makepkg --sign -df
+cd ../../linux317*/*nvidia*304*
 makepkg --sign -df
