b261af4f44a14db05deb99e17a4982ab5175b458
Philip <philm@manjaro.org>
2015-01-22 18:40:33 +0100

    Modifications - depreciate lirc - extramodules: rebuild against linux 3.19-rc5 - pamac: fix dbus issue - thus: bump to 0.8.12-rc1

--- linux313-extramodules/build-extramodules.sh
+++ linux313-extramodules/build-extramodules.sh
@@ -14,14 +14,8 @@ cd ${pwd}/*acpi_call && makepkg --sign -sf --noconfirm
 cd ${pwd}/*bbswitch && makepkg --sign -sf --noconfirm
 cd ${pwd}/*broadcom-wl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*catalyst && makepkg --sign -df --noconfirm
-cd ${pwd}/*catalyst-13.4 && makepkg --sign -df --noconfirm
-cd ${pwd}/*catalyst-legacy && makepkg --sign -df --noconfirm
-#cd ${pwd}/*cdfs && makepkg --sign -sf --noconfirm
-#cd ${pwd}/*fcpci && makepkg --sign -sf --noconfirm
-#cd ${pwd}/*fcpcmcia && makepkg --sign -sf --noconfirm
-cd ${pwd}/*lirc && makepkg --sign -sf --noconfirm
 cd ${pwd}/*ndiswrapper && makepkg --sign -sf --noconfirm
-cd ${pwd}/*nvidia && makepkg --sign -d --noconfirm
+cd ${pwd}/*nvidia && makepkg --sign -sf -d --noconfirm
 cd ${pwd}/*nvidia-304xx && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidia-340xx && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
@@ -29,9 +23,6 @@ cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
 cd ${pwd}/*spl_zfs && makepkg --sign -sf --noconfirm
 cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
-#if [ "$(uname -m)" == 'i686' ] ; then
-#   cd ${pwd}/*slmodem && makepkg --sign -sf --noconfirm
-#fi
 cd ${pwd}/*tp_smapi && makepkg --sign -sf --noconfirm
 cd ${pwd}/*vhba-module && makepkg --sign -sf --noconfirm
 cd ${pwd}/*virtualbox-modules && makepkg --sign -sf --noconfirm
