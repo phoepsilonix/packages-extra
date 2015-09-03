d40906fc3ad17ed69c4e4163fa12879eeb2a2628
Philip <philm@manjaro.org>
2015-09-03 23:28:13 +0200

    extramodules: update rt3562sta - note that linux312 driver is broken since 3.12.45 (see #23)

--- build-rt3562sta-modules.sh
+++ build-rt3562sta-modules.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 rm -Rf */*rt3562sta/{src,pkg}
-cd linux34*/*rt3562sta
-makepkg --sign -df
-cd ../../linux38*/*rt3562sta
-makepkg --sign -df
-#cd ../../linux39*/*rt3562sta
-#makepkg --sign -df
-cd ../../linux310*/*rt3562sta
-makepkg --sign -df
-cd ../../linux311*/*rt3562sta
+cd linux310*/*rt3562sta
 makepkg --sign -df
 cd ../../linux312*/*rt3562sta
 makepkg --sign -df
 cd ../../linux313*/*rt3562sta
 makepkg --sign -df
 cd ../../linux314*/*rt3562sta
+makepkg --sign -df
+cd ../../linux316*/*rt3562sta
+makepkg --sign -df
+cd ../../linux318*/*rt3562sta
+makepkg --sign -df
+cd ../../linux319*/*rt3562sta
+makepkg --sign -df
+cd ../../linux40*/*rt3562sta
+makepkg --sign -df
+cd ../../linux41*/*rt3562sta
+makepkg --sign -df
+cd ../../linux42*/*rt3562sta
 makepkg --sign -df
