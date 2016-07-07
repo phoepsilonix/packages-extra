e17b6f1d6c8a5401dc70221a190297f3c53136f2
Philip <philm@manjaro.org>
2016-07-07 23:01:50 +0200

    Modifications - extramodules: rebuild virtualbox - openchrome: update driver to latest version

--- build-virtualbox-modules.sh
+++ build-virtualbox-modules.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 rm -Rf */*virtualbox-modules/{src,pkg}
 cd linux310*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux312*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux313*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux314*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux316*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux318*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux319*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 #cd ../../linux40*/*virtualbox-modules
-#makepkg --sign -df
+#makepkg -df
 cd ../../linux41*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux42*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 #cd ../../linux43*/*virtualbox-modules
-#makepkg --sign -df
+#makepkg -df
 cd ../../linux44*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux45*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux46*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
 cd ../../linux47*/*virtualbox-modules
-makepkg --sign -df
+makepkg -df
