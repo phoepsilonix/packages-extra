7a201f81b2733687023f630e46c039423d715eb7
Bernhard Landauer <oberon@manjaro.org>
2016-08-21 15:06:40 +0200

    upd chrootbuild-virtualbox.sh

--- chrootbuild-virtualbox-modules.sh
+++ chrootbuild-virtualbox-modules.sh
@@ -4,11 +4,10 @@ build(){
   buildpkg -sp virtualbox-modules &&
   buildpkg -sp virtualbox-modules -a i686
 }
+
 cd linux310*
 build
 cd ../linux312*
-build
-cd ../linux313*
 build
 cd ../linux314*
 build
@@ -16,15 +15,9 @@ cd ../linux316*
 build
 cd ..linux318*
 build
-cd ../linux319*
-build
 cd ../linux41*
 build
-cd ../linux42*
-build
 cd ../linux44*
-build
-cd ../linux45*
 build
 cd ../linux46*
 build
